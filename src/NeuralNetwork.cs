50969660135b24329dff04fa4ffb113f9a47e966
Mattia Shux <shuxdev@gmail.com>
2022-08-29 22:35:52 +0200

    NeuralNetwork

---
+++ src/NeuralNetwork.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+
+
+namespace ShipML
+{
+    public class NeuralNetwork
+    {
+        private int[] _layersNeuronsCount;
+        private Matrix<float>[] _weights;
+        private Vector<float>[] _biases;
+        
+
+        public NeuralNetwork(params int[] neuronsCount)
+        {
+            _layersNeuronsCount = neuronsCount;
+
+            _weights = new Matrix<float>[neuronsCount.Length - 1];
+            _biases = new Vector<float>[neuronsCount.Length - 1];
+
+            for (int i = 0; i < neuronsCount.Length - 1; i++)
+            {
+                _weights[i] = Matrix<float>.Build.Random(neuronsCount[i+1], neuronsCount[i]);
+                _biases[i] = Vector<float>.Build.Dense(neuronsCount[i+1]);
+            }
+        }
+
+        public Vector<float> FeedForward(Vector<float> input)
+        {
+            Vector<float> temp = input;
+
+            for (int i = 0; i < _layersNeuronsCount.Length - 1; i++)
+            {
+                temp = _weights[i] * temp + _biases[i];
+                
+                temp.Map(x => SpecialFunctions.Logistic(x), Zeros.Include);
+
+            }
+
+            return temp;
+        }
+
+    }
+}
--- src/ShipNeuralNetwork.cs
+++ src/ShipNeuralNetwork.cs
@@ -2,7 +2,6 @@
 
 using Godot;
 
-using MathNet.Numerics;
 using MathNet.Numerics.LinearAlgebra;
 
 
@@ -14,22 +13,15 @@ namespace ShipML
         private const float MaxAngle = (float) Math.PI / 2;
         
         private const int InputLayerSize = 7;
+        private const int FirstLayerSize = 9;
+        private const int SecondLayerSize = 9;
         private const int OutputLayerSize = 4;
-        
-        private const int FirstLayerSize = 8;
-        private const int SecondLayerSize = 8;
 
         private Vector<float> _input = Vector<float>.Build.Dense(InputLayerSize);
         private Vector<float> _output = Vector<float>.Build.Dense(OutputLayerSize);
 
-        private Matrix<float> _weights01 = Matrix<float>.Build.Random(FirstLayerSize, InputLayerSize);
-        private Matrix<float> _weights12 = Matrix<float>.Build.Random(SecondLayerSize, FirstLayerSize);
-        private Matrix<float> _weights23 = Matrix<float>.Build.Random(OutputLayerSize, SecondLayerSize);
-
-        private Vector<float> _bias1 = Vector<float>.Build.Dense(FirstLayerSize);
-        private Vector<float> _bias2 = Vector<float>.Build.Dense(SecondLayerSize);
-        private Vector<float> _bias3 = Vector<float>.Build.Dense(OutputLayerSize);
-
+        private NeuralNetwork _network;
+        
         private Ship _ship;
         public Ship Ship
         {
@@ -40,32 +32,24 @@ namespace ShipML
             }
         }
 
+        public override void _Ready()
+        {
+            _network = new NeuralNetwork(InputLayerSize, FirstLayerSize, SecondLayerSize, OutputLayerSize);
+        }
+
         public override void _PhysicsProcess(float delta)
         {
             if (_ship is null) return;
 
             SetInputFromShip();
-            FeedForward(_input);
-        }
-
-        private void FeedForward(Vector<float> input)
-        {
-            var temp = _weights01 * input + _bias1;
-            temp.Map(x => SpecialFunctions.Logistic(x), Zeros.Include);
-
-            temp = _weights12 * temp + _bias2;
-            temp.Map(x => SpecialFunctions.Logistic(x), Zeros.Include);
-
-            _output = _weights23 * temp + _bias3;
-            _output.Map(x => SpecialFunctions.Logistic(x), Zeros.Include);
-
+            _output = _network.FeedForward(_input);
             SetShipOutput();
         }
 
         private void SetInputFromShip()
         {
-            _input[0] = 512;
-            _input[1] = 300;
+            _input[0] = 512 - _ship.Position.x;
+            _input[1] = 300 - _ship.Position.y;
             _input[2] = _ship.LinearVelocity.x;
             _input[3] = _ship.LinearVelocity.y;
             _input[4] = (float) Math.Cos(_ship.GlobalRotation);
